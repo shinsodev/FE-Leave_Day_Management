be2becc94d0af2439b801fbb016c4f0c853f7ad0
lucidjug <tuuju2@gmail.com>
2025-03-22 11:22:25 +0700

    interface_employees_list

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { ToastContainer } from "react-toastify";
 import SidebarLayout from "./components/layout/SidebarLayout";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import RequestLeave from "./pages/RequestLeave/RequestLeave";
+import EmployeesList from "./pages/EmployeesList/EmployeesList";
 
 // Pages
 // import Login from "./pages/Login/Login";
@@ -39,10 +40,21 @@ function App() {
         {/* <Route path="/" element={<HomePage />} /> */}
 
         <Route
-          path="/"
+          path="/request-leave"
           element={
             <SidebarLayout>
               <RequestLeave />
+
+            </SidebarLayout>
+          }
+        />
+
+        <Route
+          path="/employees-list"
+          element={
+            <SidebarLayout>
+              <EmployeesList />
+
             </SidebarLayout>
           }
         />
